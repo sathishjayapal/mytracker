918b4301086374b512155f02c950c8e0e7e93d01
Sathish Jayapal <203055+sathishjayapal@users.noreply.github.com>
2024-05-30 21:18:17 -0500

    cqrs framework details

---
+++ sathishruns-common/src/main/java/me/sathish/common/sathishrunscommon/SathishrunsCommonApplication.java
@@ -0,0 +1,13 @@
+package me.sathish.common.sathishrunscommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SathishrunsCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SathishrunsCommonApplication.class, args);
+	}
+
+}
---
+++ sathishruns-cqrs/src/test/java/me/sathish/runs/cqrs/sathishrunscqrs/SathishrunsCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sathish.runs.cqrs.sathishrunscqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SathishrunsCqrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
