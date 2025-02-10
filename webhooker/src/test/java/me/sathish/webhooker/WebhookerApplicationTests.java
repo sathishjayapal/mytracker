d7cf8822c899b7d2cbcf2885f21c4941b980c7b5
Sathish Jayapal <sjayapal@skminfotech.com>
2025-02-09 20:36:29 -0600

    Everything related to mytracker

---
+++ webhooker/src/test/java/me/sathish/webhooker/WebhookerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sathish.webhooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
