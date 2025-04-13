f14473c22df8cc6f7ca705d30cb33fa1cbacaafa
Sathish Jayapal <203055+sathishjayapal@users.noreply.github.com>
2025-04-13 18:15:54 -0500

    Spotless changes

--- sathishaccount-query/src/main/java/me/sathish/query/sathishaccountquery/repository/SathishRunDataRepository.java
+++ sathishaccount-query/src/main/java/me/sathish/query/sathishaccountquery/repository/SathishRunDataRepository.java
@@ -1,13 +1,8 @@
 package me.sathish.query.sathishaccountquery.repository;
 
 import me.sathish.query.sathishaccountquery.domain.SathishRunData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
-
 @Repository
-public interface SathishRunDataRepository extends CrudRepository<SathishRunData, Long> {
-}
+public interface SathishRunDataRepository extends CrudRepository<SathishRunData, Long> {}
--- sathishruns-common/src/main/java/me/sathish/common/sathishrunscommon/fileuploader/BlobFileUploader.java
+++ sathishruns-common/src/main/java/me/sathish/common/sathishrunscommon/fileuploader/BlobFileUploader.java
@@ -4,36 +4,36 @@ import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.azure.storage.blob.models.BlobContainerItem;
-import lombok.RequiredArgsConstructor;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 @Component
 @Slf4j
 public class BlobFileUploader {
     private List<BlobContainerItem> containersList = List.of();
     private final BlobServiceClient blobServiceClient;
+
     @Value("${file.path}")
     private String filePath;
+
     private final Environment environment;
+
     public BlobFileUploader(Environment environment) {
         this.environment = environment;
         String connectStr = environment.getProperty("azure.storage.connection-string");
-        blobServiceClient = new BlobServiceClientBuilder().connectionString(connectStr).buildClient();
+        blobServiceClient =
+                new BlobServiceClientBuilder().connectionString(connectStr).buildClient();
         try {
             containersList = blobServiceClient.listBlobContainers().stream().toList();
         } catch (Exception e) {
             log.error("Error in BlobFileUploader constructor");
         }
-
     }
 
     public static boolean fileExists(String filePath) {
@@ -41,7 +41,7 @@ public class BlobFileUploader {
     }
 
     void listBlobs() {
-        if (containersList .isEmpty()) {
+        if (containersList.isEmpty()) {
             log.error("No blobs to list. No containers found");
             return;
         }
@@ -49,26 +49,30 @@ public class BlobFileUploader {
             BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(container.getName());
             containerClient.listBlobs().forEach(blob -> {
                 log.error("Blob name: {}", blob.getName());
-                log.error("Blob URL: {}", containerClient.getBlobClient(blob.getName()).getBlobUrl());
+                log.error(
+                        "Blob URL: {}",
+                        containerClient.getBlobClient(blob.getName()).getBlobUrl());
             });
             log.error("Container name: {}", container.getName());
         }
     }
 
     void uploadActivity() {
-        if (containersList .isEmpty()) {
+        if (containersList.isEmpty()) {
             log.error("No uploads as - No containers found");
             return;
         }
         if (fileExists(filePath)) {
             containersList.stream().findFirst().ifPresent(container -> {
                 BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(container.getName());
-                boolean blobExists = containerClient.listBlobs().stream()
-                        .anyMatch(blob -> blob.getName().equalsIgnoreCase(Paths.get(filePath).getFileName().toString()));
+                boolean blobExists = containerClient.listBlobs().stream().anyMatch(blob -> blob.getName()
+                        .equalsIgnoreCase(Paths.get(filePath).getFileName().toString()));
                 if (blobExists) {
                     log.error("Blob already uploaded");
                 } else {
-                    containerClient.getBlobClient(Paths.get(filePath).getFileName().toString()).uploadFromFile(filePath, false);
+                    containerClient
+                            .getBlobClient(Paths.get(filePath).getFileName().toString())
+                            .uploadFromFile(filePath, false);
                 }
             });
         } else {
