48906694686d7e36e1653a80a1b9eeb222600b34
Sathish Jayapal <203055+sathishjayapal@users.noreply.github.com>
2025-04-13 15:49:37 -0500

    pom.xml changes

---
+++ event-service/src/main/java/me/sathish/eventservice/events/data/DomainEventMapper.java
@@ -0,0 +1,36 @@
+package me.sathish.eventservice.events.data;
+
+import me.sathish.eventservice.domain.data.Domain;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DomainEventMapper {
+    public DomainEvent toEntity(DomainEventDTO dto, Domain domain) {
+        if (dto == null || domain == null) {
+            return null;
+        }
+
+        DomainEvent domainEvent = new DomainEvent();
+        domainEvent.setId(dto.getId());
+        domainEvent.setEventId(dto.getEventId());
+        domainEvent.setEventType(dto.getType());
+        domainEvent.setPayload(dto.getPayload());
+        domainEvent.setDomainName(domain);
+        return domainEvent;
+    }
+
+    public DomainEventDTO toDto(DomainEvent entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        DomainEventDTO dto = new DomainEventDTO();
+        dto.setId(entity.getId());
+        dto.setEventId(entity.getEventId());
+        dto.setType(entity.getEventType());
+        dto.setPayload(entity.getPayload());
+        dto.setDomainName(entity.getDomainName().getDomainName());
+        dto.setEventRecordedAt(entity.getCreatedAt());
+        return dto;
+    }
+}
