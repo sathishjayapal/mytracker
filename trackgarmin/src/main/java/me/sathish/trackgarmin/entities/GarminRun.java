d0c42e240a2253f03aa641a2984f03104936ee2b
Sathish Jayapal <sjayapal@skminfotech.com>
2025-05-04 22:25:17 -0500

    UserDetails added

--- trackgarmin/src/main/java/me/sathish/trackgarmin/entities/GarminRun.java
+++ trackgarmin/src/main/java/me/sathish/trackgarmin/entities/GarminRun.java
@@ -2,12 +2,18 @@ package me.sathish.trackgarmin.entities;
 
 import jakarta.persistence.*;
 import java.math.BigInteger;
+import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.Hibernate;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Entity
 @Table(name = "garmin_runs", schema = "runs_schema")
@@ -15,7 +21,7 @@ import org.hibernate.Hibernate;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class GarminRun extends GarminMSBaseEntity {
+public class GarminRun {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -44,6 +50,22 @@ public class GarminRun extends GarminMSBaseEntity {
 
     @Column(nullable = false)
     private String activityName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "created_by", nullable = false)
+    private User createdBy;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(nullable = false, name = "created_at")
+    private Instant createdAt;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private BigInteger updatedBy;
 
     @Override
     public boolean equals(Object o) {
