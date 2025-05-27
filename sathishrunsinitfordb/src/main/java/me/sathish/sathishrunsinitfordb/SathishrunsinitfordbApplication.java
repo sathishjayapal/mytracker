c8549bf00c167131f5e777fd8f8ef7813df901d6
Sathish Jayapal <sjayapal@skminfotech.com>
2025-05-27 06:31:44 -0500

    k8s updates

--- sathishrunsinitfordb/src/main/java/me/sathish/sathishrunsinitfordb/SathishrunsinitfordbApplication.java
+++ sathishrunsinitfordb/src/main/java/me/sathish/sathishrunsinitfordb/SathishrunsinitfordbApplication.java
@@ -1,10 +1,15 @@
 package me.sathish.sathishrunsinitfordb;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SathishrunsinitfordbApplication {
+public class SathishrunsinitfordbApplication implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Database initialized");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SathishrunsinitfordbApplication.class, args);
